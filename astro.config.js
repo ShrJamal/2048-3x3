eab6f5bc4a0021ded42f5552dda5a3112af69020
Jamal Shr <me@shrjamal.com>
2024-03-25 12:51:36 +0000

    deploy to github pages

--- astro.config.js
+++ astro.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config'
-import { loadEnv } from 'vite'
 import tailwind from '@astrojs/tailwind'
 import metaTags from 'astro-meta-tags'
 import svelte, { vitePreprocess } from '@astrojs/svelte'
 
-const env = loadEnv(process.env.NODE_ENV, process.cwd(), '')
-
 // https://astro.build/config
 export default defineConfig({
-  site: env.SITE,
+  site: 'https://shrjamal.github.io',
+  base: '2048-3x3',
   integrations: [
     tailwind(),
     metaTags(),
